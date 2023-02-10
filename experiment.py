dd6bba093613b9081cd4b2887433208717a38b8d
sagar <sagarkhatri.pro@gmail.com>
2023-02-10 20:35:05 +0530

    added spam.py

--- experiment.py
+++ experiment.py
@@ -1,7 +1,2 @@
-class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        numsNew = []
-        for i in range(len(nums)):
-            if val != nums[i]:
-                numsNew.append(nums[i])
-        nums = numsNew
+var1 = "h"
+var2 = 55
